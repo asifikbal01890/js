d344e38248a56a9e8054183a390eae73eb607d20
asifikbal01890 <asifikbal01890@gmail.com>
2024-06-25 17:09:37 +0600

    first commit

---
+++ first.js
@@ -0,0 +1,32 @@
+// variable
+
+// number
+var price = 2000;
+var gf_age = 20;
+
+// string
+
+var friend = "jony ahmed";
+var age = "20";
+var address = "rayna, laxmikul, baraigram, natore";
+var name = 'raju';
+var bro = `sajib`;
+
+// Boolean
+
+var isMale = true;
+var isFemale = false;
+
+// don't use this
+ var my friend = 'asif';
+ var my-friend = 'asif';
+ var 01friend = 'asif';
+ var "price" = 2000;
+
+// use like this
+var myfriend = 'asif';
+var my_friend = 'asif';  //shanke case
+var myFriend = 'asif'; // camel case
+
+
+var areYouGoToSchool = true; // camel case
